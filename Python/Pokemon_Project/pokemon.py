5b39bb3c38cd2c921e03efbe447fa3eb08663447
Caroline McCormick <cmccormick1@email.arizona.edu>
2020-03-26 17:30:46 -0700

    Added clarifying comments and documentation

--- Python/Pokemon_Project/pokemon.py
+++ Python/Pokemon_Project/pokemon.py
@@ -6,6 +6,16 @@
     Pokemon, organizes this data, calculates average values for each category
     of data, and processes queries provided by the user, printing out the
     results.
+    
+    ------------------------------------------------------------------------
+    Example file input:
+    #  Name	        Type 1	 Type 2	Total	HP	Attack	Defense	Sp. Atk	Sp. Def	Speed	Generation	Legendary
+    1  Bulbasaur    Grass	 Poison	318	    45	49	    49	        65	    65	45	    1	        FALSE
+    2  Ivysaur	    Grass	 Poison	405	    60	62	    63	        80	    80	60	    1	        FALSE
+    3  Venusaur     Grass	 Poison	525	    80	82	    83	        100	    100	80	    1	        FALSE
+    4  Charmander	Fire	        309	    39	52	    43	        60	    50	65	    1	        FALSE
+    5  Charmeleon	Fire		    405	    58	64	    58	        80	    65	80	    1	        FALSE
+
 """
 
 def main():
@@ -17,24 +27,20 @@ def main():
 
 def create_dataset():
     """
-    This function sorts information from a file containing Pokemon fighting
+    Purpose: This function sorts information from a file containing Pokemon fighting
     statistics based on the types of Pokemon and the individual Pokemon. 
 
     Parameters: None
 
-    Returns: Poke_dict is a 2D dictionary that sorts all of the Pokemon of a
+    Returns: poke_dict is a 2D dictionary that sorts all of the Pokemon of a
     each type, then all of the statistics in a list for each Pokemon.
-    Len(stats_list) is the number of statistics that the Pokemon have.
-
-    Pre-condition: The user must provide a valid Pokemon file in CSV format.
-  
-    Post-condition: The information in the file will be categorized as
-    described above.
+    len(stats_list) is the number of statistics that the Pokemon have.
     """
     data_file = open(input()).readlines()
     poke_dict = {}
     for line in data_file:
         data = line.split(',')
+        #remove unnecessary columns in data table
         data.remove(data[0])
         data.remove(data[2])
         data.remove(data[-2])
@@ -42,7 +48,9 @@ def create_dataset():
         if data[0].lower() != 'name':
             name = data[0]
             poke_type = data[1]
+            #create list of statistic values per row
             stats_list = [data[i] for i in range(2, len(data))]
+            #add statistics list to dictionary according to Pokemon type and name
             if poke_type in poke_dict.keys():
                 poke_dict[poke_type][name] = stats_list
             else:
@@ -52,30 +60,28 @@ def create_dataset():
 
 def calculate_averages(poke_dict, num_of_stats):
     """
-    This function calculates and stores the average value across all of the
+    Purpose: This function calculates and stores the average value across all of the
     Pokemon in each type of Pokemon for each category in the statistics list.
 
-    Parameters: Poke_dict provides the sorted file of all the Pokemon information.
-    Num_of_stats is used to indicate how many categories of statistics are in
+    Parameters: poke_dict provides the sorted file of all the Pokemon information.
+    num_of_stats is used to indicate how many categories of statistics are in
     the data file.
 
-    Returns: Average_dict is a dictionary that has the Pokemon type as the keys
+    Returns: average_dict is a dictionary that has the Pokemon type as the keys
     and a list of the average values for each statistic for all the Pokemon of
     that type as the values.
-
-    Pre-condition: None
-  
-    Post-condition: The average values can now be compared to determine the
-    highest average value for each statistic category over the whole file.
     """
     average_dict = {}
+    #for each Pokemon type
     for poke_type in poke_dict.keys():
         poke_stats_dict = poke_dict[poke_type]
         type_averages = []
         for i in range(0, num_of_stats):
             total = 0
+            #for each statistic column, add together the values
             for stats_list in poke_stats_dict.values():
                 total += int(stats_list[i])
+            #calculate average of statistic across one type of Pokemon 
             type_averages.append(total / len(poke_stats_dict))
         average_dict[poke_type] = type_averages
     return average_dict
@@ -83,31 +89,27 @@ def calculate_averages(poke_dict, num_of_stats):
 
 def highest_averages(average_dict, num_of_stats):
     """
-    This function determines the highest average value for each category of
+    Purpose: This function determines the highest average value for each category of
     statistic over every type of Pokemon.
 
-    Parameters: Average_dict is a dictionary that has the Pokemon type as the
+    Parameters: average_dict is a dictionary that has the Pokemon type as the
     keys and a list of the average values for each statistic for all the Pokemon
-    of that type as the values. Num_of_stats is used to indicate how many
+    of that type as the values. num_of_stats is used to indicate how many
     categories of statistics are in the data file.
 
-    Returns: Max_avg_dict is a dictionary that has the categories of statistics
+    Returns: max_avg_dict is a dictionary that has the categories of statistics
     as the keys and an inner dictionary as the values that contains the Pokemon
     type mapped to the corresponding maximum average for that category. 
-
-    Pre-condition: None
-  
-    Post-condition: The maximum average for each category along with the type
-    of Pokemon at that maximum average is stored so that it can be referenced
-    depending on what category the user inputs.
     """
     max_avg_dict = {}
     categories = ['Total', 'HP', 'Attack', 'Defense', 'SpecialAttack',
                   'SpecialDefense', 'Speed']
+    #for each statistical category
     for i in range(0, num_of_stats):
         pokemon_type = ''
         max_average = 0
         output = {}
+        #find the maximum average value for a statistic across all Pokemon types
         for poke_type,type_averages in average_dict.items():
             if type_averages[i] > max_average:
                 max_average = type_averages[i]
@@ -121,34 +123,22 @@ def highest_averages(average_dict, num_of_stats):
 
 def query(max_avg_dict):
     """
-    This function repeatedly prompts the user who inputs a category of
+    Purpose: This function repeatedly prompts the user who inputs a category of
     statistic, and it prints out the maximum average and the type of Pokemon
     that has the maximum average.
 
-    Parameters: Max_avg_dict is a dictionary that has the categories of
+    Parameters: max_avg_dict is a dictionary that has the categories of
     statistics as the keys and an inner dictionary as the values that contains
     the Pokemon type mapped to the corresponding maximum average for that
     category.
 
     Returns: None
-
-    Pre-condition: The user must input a valid category of statistic to expect
-    values printed out.
-  
-    Post-condition: The maximum average value and the type of Pokemon at that
-    value is printed out based on the category that the user inputs. If there
-    is a tie in maximum average values, all are printed out in alphabetical
-    order based on the Pokemon type.
     """
-    user_query = input()
+    user_query = input('Category? ')
     while user_query != '':
         for category in max_avg_dict.keys():
             if user_query.lower() == category.lower():
                 output = max_avg_dict[category]
                 for poke_type in sorted(output.keys(), key=str.lower):
                     print("{}: {}".format(poke_type, str(output[poke_type])))
-        user_query = input()
-        
-
-
-main()
+        user_query = input('Category? ')
